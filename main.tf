5c1cd5d7b8e6566985d3042e0db9b040abf6a05e
Ajit Jadhav <anushka.ajit-jadhav@capgemini.com>
2025-05-13 22:08:01 +0530

    adding files of mini project

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-east-1"
+}
+ 
+terraform {
+  backend "s3" {
+    bucket = "demo-bucket-anushka26"
+    key    = "terraform/state.tfstate"
+    region = "us-east-1"
+  }
+}
+ 
+resource "aws_s3_bucket" "tfstate_bucket" {
+  bucket = "demo-bucket-anushka26"
+}
+ 
+resource "aws_instance" "app_server" {
+  ami           = "ami-0f88e80871fd81e91" # Amazon Linux 2
+  instance_type = "t2.micro"
+ 
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y git
+curl -sL https://rpm.nodesource.com/setup_16.x | bash -
+    yum install -y nodejs
+git clone https://github.com/your-username/your-repo.git
+    cd your-repo
+    node index.js
+  EOF
+ 
+  tags = {
+    Name = "Terraform-EC2-Instance"
+  }
+}
