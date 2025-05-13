d3d5518a76444b04f4721a7176bec8c54bd9eeae
jAnushka26 <ANUSHKA.AJIT-JADHAV@CAPGEMINI.COM>
2025-05-13 23:20:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,6 @@ terraform {
   }
 }
  
-resource "aws_s3_bucket" "tfstate_bucket" {
-  bucket = "demo-bucket-anushka26"
-}
- 
 resource "aws_instance" "app_server" {
   ami           = "ami-0f88e80871fd81e91" # Amazon Linux 2
   instance_type = "t2.micro"
