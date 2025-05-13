0b0599952e57b85326bd9485567ef6b46d2a35a9
Ajit Jadhav <anushka.ajit-jadhav@capgemini.com>
2025-05-13 23:42:44 +0530

    modified main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
- 
+
 terraform {
   backend "s3" {
     bucket = "demo-bucket-anushka26"
@@ -9,26 +9,17 @@ terraform {
     region = "us-east-1"
   }
 }
- 
+
 resource "aws_s3_bucket" "tfstate_bucket" {
   bucket = "demo-bucket-anushka26"
 }
- 
+
 resource "aws_instance" "app_server" {
   ami           = "ami-0f88e80871fd81e91" # Amazon Linux 2
   instance_type = "t2.micro"
- 
-  user_data = <<-EOF
-    #!/bin/bash
-    yum update -y
-    yum install -y git
-curl -sL https://rpm.nodesource.com/setup_16.x | bash -
-    yum install -y nodejs
-git clone https://github.com/jAnushka26/jenkins-pipeline-2.0.git
-    cd node_app
-    node index.js
-  EOF
- 
+
+  user_data = file("setup.sh")
+
   tags = {
     Name = "Terraform-EC2-Instance"
   }
